51d2d0968d66947c2a769d0339133219d813f133
Miraj Vyas <62197800+mirajvyas99@users.noreply.github.com>
2021-02-18 00:21:51 +0530

    Add files via upload

---
+++ lib/src/screens/main_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+//Pages
+import '../pages/home_page.dart';
+import '../pages/order_page.dart';
+import '../pages/favorite_page.dart';
+import '../pages/profile_page.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentTabIndex = 0;
+
+  List<Widget> pages;
+  Widget currentPage;
+
+  HomePage homePage;
+  OrderPage orderPage;
+  FavoritePage favoritePage;
+  ProfilePage profilePage;
+
+  @override
+  void initState() {
+    super.initState();
+    homePage = HomePage();
+    orderPage = OrderPage();
+    favoritePage = FavoritePage();
+    profilePage = ProfilePage();
+    pages = [
+      homePage,
+      orderPage,
+      favoritePage,
+      profilePage,
+    ];
+    currentPage = homePage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            currentTabIndex = index;
+            currentPage = pages[index];
+          });
+        },
+        currentIndex: currentTabIndex,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text("Orders"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: Text("favorite"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+      body: currentPage,
+    );
+  }
+}
