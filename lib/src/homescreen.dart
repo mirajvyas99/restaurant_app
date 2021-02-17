35ccf8750ca1b3902ad6591ec1a5dd825e7657d5
Miraj Vyas <62197800+mirajvyas99@users.noreply.github.com>
2021-02-18 00:26:19 +0530

    Add files via upload

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'widgets/bought_foods.dart';
+import 'widgets/bought_foods.dart';
 import 'widgets/food_category.dart';
 import 'widgets/home_top_info.dart';
 import 'widgets/search_field.dart';
+import 'widgets/bought_foods.dart';
+
+//Data
+import 'data/food_data.dart';
+
+//Model
+import 'models/food_model.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,33 +19,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Food> _foods = foods;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.white,
       body: ListView(
-        padding: EdgeInsets.only(top: 50,left: 20,right: 20),
+        padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         children: <Widget>[
           HomeTopInfo(),
           FoodCategory(),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20.0,
+          ),
           SearchField(),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20.0,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text("Frequently Bought Foods",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.white),),
+              Text(
+                "Frequently Bought Foods",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
               GestureDetector(
-                onTap: (){},
+                onTap: () {},
                 child: Text(
                   "View All",
-                  style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold, color: Colors.orangeAccent),
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orangeAccent),
                 ),
               ),
             ],
           ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Column(
+            children: _foods.map(_buildFoodItems).toList(),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildFoodItems(Food food) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20.0),
+      child: BoughtFoods(
+        id: food.id,
+        name: food.name,
+        imagePath: food.imagePath,
+        category: food.category,
+        discount: food.discount,
+        price: food.price,
+        ratings: food.ratings,
       ),
     );
   }
