3927cd0c0f81f497104e6b167848d58300cf417b
mirajvyas99 <170120107187@git.org.in>
2021-02-19 12:06:21 +0530

    after data commit PC

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import "src/app.dart";
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
