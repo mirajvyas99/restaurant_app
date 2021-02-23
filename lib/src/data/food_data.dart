ba48d707602e5b8a4fddfa03f7d62b857c3fc0de
mirajvyas99 <62197800+mirajvyas99@users.noreply.github.com>
2021-02-24 00:34:12 +0530

    sub commit

--- lib/src/data/food_data.dart
+++ lib/src/data/food_data.dart
@@ -3,8 +3,8 @@ import '../models/food_model.dart';
 final foods = [
   Food(
     id: "1",
-    name: "Hot Coffee",
-    imagePath: "assets/images/breakfast.jpeg",
+    name: "Paneer Butter Masala",
+    imagePath: "assets/images/paneerbuttermasala.jpg",
     category: "1",
     price: 22.0,
     discount: 33.5,
@@ -12,8 +12,8 @@ final foods = [
   ),
   Food(
     id: "1",
-    name: "Grilled Chicken",
-    imagePath: "assets/images/lunch.jpeg",
+    name: "BBQ Paneer",
+    imagePath: "assets/images/starter2.jpg",
     category: "2",
     price: 12.0,
     discount: 34.5,
@@ -30,8 +30,8 @@ final foods = [
   ),
   Food(
     id: "1",
-    name: "Chicken Biryani",
-    imagePath: "assets/images/supper_1.jpeg",
+    name: "Chocolate Brownie Volcano",
+    imagePath: "assets/images/volcano.jpg",
     category: "4",
     price: 25.0,
     discount: 10.5,
