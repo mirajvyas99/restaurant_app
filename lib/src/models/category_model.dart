77b2c6688c436ee3f83c3c4c29d46d2c37e91a0e
mirajvyas99 <62197800+mirajvyas99@users.noreply.github.com>
2021-03-10 23:11:18 +0530

    Auth: user sign-up commit

--- lib/src/models/category_model.dart
+++ lib/src/models/category_model.dart
@@ -1,7 +1,7 @@
-class Category{
+class Category {
   final String categoryName;
-  final String  imagePath;
+  final String imagePath;
   final int numberOfItems;
 
-  Category({this.categoryName,this.imagePath,this.numberOfItems});
+  Category({this.categoryName, this.imagePath, this.numberOfItems});
 }
