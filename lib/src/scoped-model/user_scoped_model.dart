b958bf539b4f9768394b64b470cf398fdfee8cff
Miraj Vyas <62197800+mirajvyas99@users.noreply.github.com>
2021-03-11 11:24:39 +0530

    Update user_scoped_model.dart

--- lib/src/scoped-model/user_scoped_model.dart
+++ lib/src/scoped-model/user_scoped_model.dart
@@ -5,22 +5,71 @@ import 'package:http/http.dart' as http;
 
 class UserModel extends Model {
   User _authenticatedUser;
+  bool _isLoading = false;
 
-  void authenticate(String email, String password) async {
+  User get authenticatedUser{
+    return _authenticatedUser;
+  }
+  
+  bool get isLoading{
+    return _isLoading;
+  }
+
+  Future<Map<String, dynamic>> authenticate(String email, String password) async {
+    
+    _isLoading = true;
+    notifyListeners();
+    
     Map<String, dynamic> authData = {
       "email": email,
       "password": password,
       "returnSecureToken": true,
     };
+    String message;
+    bool hasError = false;
+
     try {
       http.Response response = await http.post(
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJMifZBzjK32fKohdlON-GdlruDRo3LuM",
         body: json.encode(authData),
         headers: {'Content-Type': 'application/json'},
       );
-      print("Printing the response body: ${response.body}");
+      
+      Map<String, dynamic> responseBody = json.decode(response.body);
+
+      if(responseBody.containsKey('idToken')){
+        _authenticatedUser = User(
+        id: responseBody['localId'],
+        email: responseBody['email'],
+        token: responseBody['idToken'],
+        userType: 'customer',
+      );
+      message = "Signed Up Successfully";
+      }else{
+        if(responseBody['error']['message'] == 'EMAIL_EXISTS'){
+          hasError = true;
+          message = "Email already exists";
+          // print("Email already exists");
+        }
+      }
+
+      print("Printing the user token: ${_authenticatedUser.token}");
+
+      _isLoading = false;
+      notifyListeners();
+      return {
+        'message': message,
+        'hasError': hasError,
+      };
     } catch (error) {
-      print("The error signing up $error");
+
+      _isLoading = false;
+      notifyListeners();
+
+      return{
+        'message': 'Failed to Sign up',
+        'hasError': !hasError,
+      };
     }
   }
 }
