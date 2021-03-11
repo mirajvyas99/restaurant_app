eebe2d98a646e20d115642c22cdb2470c7292d20
Miraj Vyas <62197800+mirajvyas99@users.noreply.github.com>
2021-03-11 11:34:43 +0530

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_app/src/scoped-model/food_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'admin/pages/add_food_item.dart';
+import 'pages/signin_page.dart';
 import 'scoped-model/main_model.dart';
 import 'screens/main_screen.dart';
 
@@ -17,6 +18,9 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: MainScreen(model: mainModel),
         // home: AddFoodItem(),
+        routes: {
+          "/" : (BuildContext context) => SignInPage(),
+        },
       ),
     );
   }
