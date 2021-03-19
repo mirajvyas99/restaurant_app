044f1878c4480f033890d26d0a5600bb45b5776b
mirajvyas99 <170120107187@git.org.in>
2021-03-19 18:51:54 +0530

    about us commit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/src/pages/about_us.dart';
-import 'package:restaurant_app/src/scoped-model/food_model.dart';
 import 'package:restaurant_app/src/screens/splash_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'admin/pages/add_food_item.dart';
-import 'pages/signin_page.dart';
 import 'scoped-model/main_model.dart';
 import 'screens/main_screen.dart';
 
