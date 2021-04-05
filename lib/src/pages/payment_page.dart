7c7125f801d3e5abb3ee1ac104ecbdc736ad1d83
mirajvyas99 <170120107187@git.org.in>
2021-04-05 18:21:47 +0530

    payment page decor commit

--- lib/src/pages/payment_page.dart
+++ lib/src/pages/payment_page.dart
@@ -6,15 +6,32 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          centerTitle: true,
+          title: Text("Payment Methods", style: TextStyle(color: Colors.black, fontSize: 20.0,fontWeight: FontWeight.bold)),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Payment Methods", style: TextStyle(color: Colors.black, fontSize: 30.0,fontWeight: FontWeight.bold),),
+              Container(
+                width: 500,
+                height:100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 1.0, color: Colors.white),
+                ),
+                child: Image.network("https://www.searchpng.com/wp-content/uploads/2019/02/Paytm-Logo-With-White-Border-PNG-image.png",fit: BoxFit.contain,),
+              ),
+              Text("Accepted Here", style: TextStyle(color: Colors.black, fontSize: 30.0)),
               SizedBox(height: 20,),
               Container(
                 width: 300,
@@ -26,24 +43,14 @@ class _PaymentPageState extends State<PaymentPage> {
                 child: Image.asset("assets/images/qrcode.png",fit: BoxFit.contain,),
               ),
               SizedBox(height: 20,),
-              Container(
-                width: 300,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 1.0, color: Colors.black),
-                ),
-                child: Text("UPI"),
-              ),
+              Text("OR", style: TextStyle(color: Colors.black, fontSize: 20.0)),
+              Text("Enter Mobile Number", style: TextStyle(color: Colors.black, fontSize: 20.0)),
               SizedBox(height: 20,),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 1.0, color: Colors.black,),
-                ),
-              )
+              Text("____________",style: TextStyle(color: Colors.black, fontSize: 20.0)),
+              SizedBox(height: 20,),
+              Text("8141418054",style: TextStyle(color: Colors.black, fontSize: 30.0)),
+              Text("____________",style: TextStyle(color: Colors.black, fontSize: 20.0)),
+              SizedBox(height: 20,),
             ],
           ),
         ),
